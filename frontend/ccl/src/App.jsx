e8c205a63f3610a0c9d68327db007df589a0b632
reza <r.eivazzadeh97@gmail.com>
2026-01-05 23:38:10 +0330

    [front-add] new home page

--- frontend/ccl/src/App.jsx
+++ frontend/ccl/src/App.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
-import MainPage from './components/MainPage/MainPage';
 import './App.css';
+import RotatingBanner from './components/RotatingBanner/RotatingBanner';
+import MatchHistory from './components/MatchHistory/MatchHistory';
+import UserPoints from './components/UserPoints/UserPoints';
+import Leaderboard from './components/LeaderBoard/LeaderBoard';
 
 function App() {
-  // In a real app, you would have routing logic here.
-  // If the user is not logged in, show the Login/Signup page.
-  // If they are, show the MainPage.
-  // For now, we'll just show the MainPage directly.
-  return <MainPage />;
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1 className="app-title">
+          <span className="neon-text">CHESS</span> ARENA
+        </h1>
+      </header>
+      
+      <main className="main-content">
+        <div className="left-column">
+          <RotatingBanner />
+          <UserPoints />
+        </div>
+        
+        <div className="center-column">
+          <Leaderboard />
+        </div>
+        
+        <div className="right-column">
+          <MatchHistory />
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
